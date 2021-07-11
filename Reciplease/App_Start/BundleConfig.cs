98acf9e079a4cf90f7dc8296760b94ee6de52ac8
brawer88 <79695807+brawer88@users.noreply.github.com>
2021-07-11 18:35:48 -0400

    got bootstrap back for us

--- Reciplease/App_Start/BundleConfig.cs
+++ Reciplease/App_Start/BundleConfig.cs
@@ -16,8 +16,8 @@ namespace Reciplease {
 			bundles.Add( new ScriptBundle( "~/bundles/modernizr" ).Include(
 						"~/Scripts/modernizr-*" ) );
 
-			//bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
-			//		  "~/Scripts/bootstrap.js" ) );
+			bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
+					  "~/Scripts/bootstrap.bundle.js" ) );
 
 			bundles.Add( new ScriptBundle( "~/bundles/jquery" ).Include(
 					  "~/Scripts/jquery-3.6.0.min.js",
