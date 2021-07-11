9f5298f6e40f6026a5b003e5da065ed670a9c960
omoh03 <86485541+omoh03@users.noreply.github.com>
2021-07-11 16:31:53 -0400

    added details to profile controller

--- Reciplease/Controllers/ProfileController.cs
+++ Reciplease/Controllers/ProfileController.cs
@@ -1,13 +1,175 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Reciplease.Models;
+using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Reciplease.Controllers
 {
     public class ProfileController
     {
+        public ActionResult Index()
+        {
+            Models.User u = new Models.User();
+            u = u.GetUserSession();
+            
+            return View(u);
+        }
 
+        [HttpPost]
+        public ActionResult Index( FormCollection collfrmAttr)
+        {
+            try
+            {
+                Models.User u = new Models.User();
+                u = u.GetUserSession();
+
+                u.FirstName = collfrmAttr["FirstName"];
+                u.LastName = collfrmAttr["LastName"];
+                u.Email = collfrmAttr["Email"];
+                u.Username = collfrmAttr["Username"];
+                u.Password = collfrmAttr["Password"];
+
+                if (u.FirstName.Length == 0 || u.LastName.Length == 0 || u.Email.Length == 0 || u.UserID.Length == 0 || u.Password.Length == 0)
+                {
+                    u.ActionType = Models.User.ActionTypes.RequiredFieldsMissing;
+                    return View(u);
+                }
+                else
+                {
+                    if (collfrmAttr["btnSubmit"] == "update")
+                    { //update button pressed
+                        u.Save();
+
+                        
+ 
+                        u.SaveUserSession();
+                        return RedirectToAction("Index");
+                    }
+                    return View(u);
+                }
+            }
+            catch (Exception)
+            {
+                Models.User u = new Models.User();
+                return View(u);
+            }
+
+        }
+
+        public ActionResult SignIn()
+        {
+            Models.User u = new Models.User();
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(FormCollection col)
+        {
+            try
+            {
+                Models.User u = new Models.User();
+
+                if (col["btnSubmit"] == "signin")
+                {
+                    u.UserID = col["UserID"];
+                    u.Password = col["Password"];
+
+                    if (u.UserID.Length == 0 || u.Password.Length == 0)
+                    {
+                        u.ActionType = Models.User.ActionTypes.RequiredFieldsMissing;
+                        return View(u);
+                    }
+                    else
+                    {
+                        u = u.Login();
+                        if (u != null && u.UID > 0)
+                        {
+                            u.SaveUserSession();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            u = new Models.User();
+                            u.UserID = col["UserID"];
+                            u.ActionType = Models.User.ActionTypes.LoginFailed;
+                        }
+                    }
+                }
+                return View(u);
+            }
+            catch (Exception)
+            {
+                Models.User u = new Models.User();
+                return View(u);
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            Models.User u = new Models.User();
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(FormCollection col)
+        {
+            try
+            {
+                Models.User u = new Models.User();
+
+                u.FirstName = col["FirstName"];
+                u.LastName = col["LastName"];
+                u.Email = col["Email"];
+                u.UserID = col["UserID"];
+                u.Password = col["Password"];
+
+                if (u.FirstName.Length == 0 || u.LastName.Length == 0 || u.Email.Length == 0 || u.UserID.Length == 0 || u.Password.Length == 0)
+                {
+                    u.ActionType = Models.User.ActionTypes.RequiredFieldsMissing;
+                    return View(u);
+                }
+                else
+                {
+                    if (col["btnSubmit"] == "signup")
+                    { //sign up button pressed
+                        Models.User.ActionTypes at = Models.User.ActionTypes.NoType;
+                        at = u.Save();
+                        switch (at)
+                        {
+                            case Models.User.ActionTypes.InsertSuccessful:
+                                u.SaveUserSession();
+                                return RedirectToAction("Index");
+                            //break;
+                            default:
+                                return View(u);
+                                //break;
+                        }
+                    }
+                    else
+                    {
+                        return View(u);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Models.User u = new Models.User();
+                return View(u);
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            Models.User u = new Models.User();
+            u.RemoveUserSession();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
+
