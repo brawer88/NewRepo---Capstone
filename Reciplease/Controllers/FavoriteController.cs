ae698531d3917ea117d5c4deb3a470886a4029d0
omoh03 <86485541+omoh03@users.noreply.github.com>
2021-07-15 14:58:43 -0400

    Created Favorite controller

---
+++ Reciplease/Controllers/FavoriteController.cs
@@ -0,0 +1,36 @@
+﻿using Reciplease.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reciplease.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public ActionResult Add_Favorite()
+        {
+            return View();
+        }
+    }
+
+    // [HttpPost]
+    //  public ActionResult Add()
+    // {
+
+    // }
+
+
+   // public ActionResult Remove_Favorite()
+   // {
+       // return RedirectToAction("Index", "Home");
+  //  }
+
+
+
+}
+     
